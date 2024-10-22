dd30d399d101d4e9aa37e9ef5664e6378060c1a0
full-stack-dhivakar <106915835+full-stack-dhivakar@users.noreply.github.com>
2024-10-22 22:45:37 +0530

    first-commit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,67 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
-const PORT = 3000;
+app.use(cors());
+app.use(express.json());
 
+const mongoDBURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/student-db';
+console.log('db connection: ', mongoDBURI);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+mongoose.connect(mongoDBURI);
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    mail: String,
+    feedback: String,
+});
+
+const userModel = mongoose.model('users', userSchema);
+
+app.post('/students', (req, res) => {
+    const formstudents = new userModel(req.body);
+    formstudents.save()
+        .then((students) => res.json(students))
+        .catch(() => res.status(500).send('data error'))
+});
+
+app.get('/students', (req, res) => {
+    userModel.find()
+        .then((data) => res.json(data))
+        .catch(() => res.status(500).send('error'))
+})
+
+app.delete('/students/:id', (req, res) => {
+    const { id } = req.params;
+
+    userModel.findByIdAndDelete(id)
+        .then((deletedData) => {
+            if (deletedData) {
+                res.json({ message: 'Data successfully deleted', deletedData });
+            } else {
+                res.status(404).json({ message: 'Data not found' });
+            }
+        })
+        .catch(() => res.status(500).send('Error deleting data'));
+});
+
+app.put('/students/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedData = req.body;
+
+    userModel.findByIdAndUpdate(id, updatedData, { new: true })
+        .then((data) => {
+            if (data) {
+                res.json({ message: 'Data successfully updated', data });
+            } else {
+                res.status(404).json({ message: 'Data not found' });
+            }
+        })
+        .catch(() => res.status(500).send('Error updating data'));
 });
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
